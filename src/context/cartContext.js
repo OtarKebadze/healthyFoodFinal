674f7817a5e73195b61bd10a3c4d7d17c3e83518
OtarKebadze <OtarKebadze@gmail.com>
2022-03-16 23:29:41 -0300

    cambios

--- src/context/cartContext.js
+++ src/context/cartContext.js
@@ -9,36 +9,22 @@ export const useCartContext = () => useContext(CartContext)
 function CartContextProv ({children}){
     const [cartList, setCartList] = useState([])
     const agregarCart = (item)=>{
-        if (existeItem(cartList !==[])){
-        cartList.forEach(e =>{
-            if (e.id === item.id) {
-            alert("ESTE ITEM YA EXISTE EN EL CARRITO")
-            return
-            }
-        })
+        if (existeId(item && item.id)){
+        alert("ESTE PRODUCTO YA EXISTE EN EL CARRITO")
+        return
         }else{
-            setCartList ([...cartList , item])
-        }
-        
-        }
+        setCartList ([...cartList , item])
+        }}
+
 
     const vaciarCart = ()=>{
     setCartList([])}
 
     function existeId(id){
-        for (let e of cartList){
-        return e.id
-        }
-    }
-    function existeItem(id){
-        if (existeId(id !== undefined)) {
-        return cartList
-        } else {
-        return false
-        }
+        const existe= cartList.find(e=>e.id === id)
+        return existe
     }
     console.log(existeId())
-    console.log(existeItem())
         return (
         <CartContext.Provider value={{
             cartList,
